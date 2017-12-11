8d9eec0a9da251798e77bff8f26d3051dbd29b3e
Gerry Morales Meza <sleep.tiger2@gmail.com>
2017-12-11 16:55:13 -0600

    2nd commit - created some data structures and accessed some data

--- anwser.js
+++ anwser.js
@@ -5,3 +5,17 @@ console.log("4> Arrays");
 console.log("5> Objects");
 console.log("6> An Objects that contain an Arrays");
 
+const rainbowColor = ["red", "organ", "yellow", "green", "blue", "indigo", "violet"];
+console.log(rainbowColor);
+
+console.log(rainbowColor[4]);
+
+const myInfo = {
+	name: "Khoa",
+	favFood: "Shirmp",
+	hobby: "Chatting",
+	town: "Austin",
+	favDataType: "Booleans"
+};
+
+console.log(myInfo.hobby);
