8adcd70f6e000e330d9c5b8ecba56ffdd18b9e2a
Gerry Morales Meza <sleep.tiger2@gmail.com>
2017-12-11 16:44:06 -0600

    1st commit - selecting datatypes

---
+++ anwser.js
@@ -0,0 +1,7 @@
+console.log("1> Booleans");
+console.log("2> String");
+console.log("3> Arrays");
+console.log("4> Arrays");
+console.log("5> Objects");
+console.log("6> An Objects that contain an Arrays");
+
